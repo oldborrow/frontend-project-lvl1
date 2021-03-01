3e903c3ae127ea98d67245f3353500c10fe8f2af
oldborrow <spevak.vlad@gmail.com>
2021-03-01 16:10:34 +0300

    eslint fix calc

--- src/calculator.js
+++ src/calculator.js
@@ -1,26 +1,24 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min))) + min;
 }
 
 const calculator = (name) => {
   let countOfRightAnswers = 0;
-  console.log("What is the result of the expression?");
+  console.log('What is the result of the expression?');
   let i = 0;
   for (i = 0; i < 3; i++) {
     const num1 = Math.floor(Math.random() * 100);
     const num2 = Math.floor(Math.random() * 100);
 
-    let ind = getRandomInt(1, 4);
+    const ind = getRandomInt(1, 4);
 
-    let sign = "";
+    let sign = '';
     let result = 0;
 
     if (ind === 1) {
-      sign = "*";
+      sign = '*';
       console.log(`Question: ${num1} ${sign} ${num2}`);
       result = num1 * num2;
     }
@@ -30,14 +28,14 @@ const calculator = (name) => {
       result = num1 - num2;
     }
     if (ind === 3) {
-      sign = "+";
+      sign = '+';
       console.log(`Question: ${num1} ${sign} ${num2}`);
       result = num1 + num2;
     }
-    const answer = readlineSync.question("Your answer: ");
+    const answer = readlineSync.question('Your answer: ');
 
     if (answer === `${result}`) {
-      console.log("Correct!");
+      console.log('Correct!');
       countOfRightAnswers += 1;
     } else {
       console.log(
