a1de9712f5ac2d19564a89cb85820ee01a2c11ad
oldborrow <spevak.vlad@gmail.com>
2021-03-01 15:40:35 +0300

    gcd fix

--- src/gcd.js
+++ src/gcd.js
@@ -13,27 +13,28 @@ function sas(a,b) {
 }
 
 const gcd = (name) => {
+    let countOfRightAnswers = 0;
     console.log("Find the greatest common divisor of given numbers.");
     let i = 0;
-    console.log(1)
     for (i = 0; i < 3; i++) {
         let num1 = Math.floor(Math.random() * 100);
         let num2 = Math.floor(Math.random() * 100);
-        console.log(2)
         console.log(`Question: ${num1} ${num2}`);
         let userAnswer = readlineSync.question('Your answer: ');
         let trueAnswer = sas(num1, num2);
         if (userAnswer === `${trueAnswer}`) {
             console.log("Correct!");
+            countOfRightAnswers += 1;
         } else {
-            console.log(4)
-            i = -1;
             console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${trueAnswer}'.)`);
-            console.log(`Let's try again, ${name}!`);
+            break;
         }
-        console.log(5)
     }
-    console.log(`Congratulations, ${name}!`);
+    if (countOfRightAnswers === 3) {
+        console.log(`Congratulations, ${name}!`);
+    } else {
+        console.log(`Let's try again, ${name}!`);
+    }
 }
 
 export default gcd;
